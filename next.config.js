3a5f0dfc35d61b5b3f7aa7650d8a3abf0f771c02
Schipsi <95927634+RealSchipsi@users.noreply.github.com>
2023-01-04 20:03:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    HOST_URL_: "https://d1fje5s5cr9ixm.cloudfront.net",
-    HOST_URL: "http://localhost:3000",
+    HOST_URL: "CLOUDFRONT_DISTRIBUTION_URL",
     MONGODB_URI:
-      "mongodb+srv://mongo-admin:Xm32Euv7@cluster0.isjyv5s.mongodb.net/?retryWrites=true&w=majority",
-    JWT_KEY: "gkP6y9ZQtmUe1FjIu46WQwWAN8MzqvKT",
+      "MONGO_DB_URI",
+    JWT_KEY: "JWT_KEY",
   },
   images: {
     domains: ["https://i.ibb.co/"],
